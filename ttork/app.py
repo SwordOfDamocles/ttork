e746291ccc415b9621f763ff38a9c20f5d2d5660
SwordOfDamocles <awaller@gmail.com>
2024-04-03 21:46:00 -0700

    UI status mockups

--- ttork/app.py
+++ ttork/app.py
@@ -13,16 +13,18 @@ from textual.widgets.tree import TreeNode
 
 TILT_STATUS_ICONS = dict(
     ok=Text.from_markup(":green_circle:"),
-    pending=Text.from_markup(":yellow_circle:", style="blink"),
+    pending=Text.from_markup(":orange_circle:", style="blink"),
     none=Text.from_markup(":black_circle:"),
-    in_progress=Text.from_markup(":orange_circle:", style="blink"),
+    in_progress=Text.from_markup(":blue_circle:", style="blink"),
     error=Text.from_markup(":red_circle:"),
+    offline=Text.from_markup(":black_circle: (offline)"),
     other=Text.from_markup(":purple_circle:"),
 )
 
 
 class TTorkApp(App):
-    """Textual Tilt ORKestration App"""
+    """Textual Tilt ORKestration App
+    """
 
     CSS_PATH = "ttork.tcss"
     BINDINGS = [
@@ -35,8 +37,17 @@ class TTorkApp(App):
         # In yaml, this will be relative: 'seeder/Tiltfile', we will
         # convert on initial load of yaml file based on cwd to absolute.
         "/Users/awaller/waller_dev/projects/rcwl/seeder/Tiltfile": dict(
-            name="Seeder", uiResources=[]
-        )
+            name="Seeder",
+            uiResources=[],
+            service_online=False,
+            port=10350,
+        ),
+        "/Users/awaller/waller_dev/projects/rcwl/feeder/Tiltfile": dict(
+            name="Feeder",
+            uiResources=[],
+            service_online=False,
+            port=10351,
+        ),
     }
 
     def compose(self) -> ComposeResult:
@@ -62,33 +73,66 @@ class TTorkApp(App):
             """
             for project_key in data:
                 project_node = root.add("")
-                label = Text.assemble(
-                    TILT_STATUS_ICONS['in_progress'],
-                    Text.from_markup(
-                        f" {data[project_key]["name"]}"
-                    ),
-                )
-                project_node.set_label(label)
                 project_node.expand()
+                project_pending = False
+                project_ok = True
                 for resource in data[project_key]['uiResources']:
                     resource_node = project_node.add("")
                     resource_node.allow_expand = False
+                    update_status = resource['status']['updateStatus']
                     label = Text.assemble(
-                        TILT_STATUS_ICONS['ok'],
+                        TILT_STATUS_ICONS.get(
+                            update_status,
+                            TILT_STATUS_ICONS['other'],
+                        ),
                         Text.from_markup(
                             f" [b]{resource['metadata']['name']}[/b]"
                         ),
                     )
                     resource_node.set_label(label)
 
+                    if (
+                        update_status == 'pending'
+                        or update_status == 'in_progress'
+                    ):
+                        project_pending = True
+                    elif update_status == 'error':
+                        project_ok = False
+
+                # Set the top-level status for the project based on resource
+                # status.
+                if not data[project_key]["service_online"]:
+                    ps_icon = TILT_STATUS_ICONS['offline']
+                elif project_pending:
+                    ps_icon = TILT_STATUS_ICONS['pending']
+                elif not project_ok:
+                    ps_icon = TILT_STATUS_ICONS['error']
+                else:
+                    ps_icon = TILT_STATUS_ICONS['ok']
+                project_label = Text.assemble(
+                    ps_icon,
+                    Text.from_markup(
+                        f" {data[project_key]["name"]}"
+                    ),
+                )
+                project_node.set_label(project_label)
+
         proper_add_node(root, project_data)
 
     # format this
     def on_mount(self) -> None:
-        status_json = self.get_tilt_status(10350)
-        self.project_info[status_json["uiSession"]["status"]["tiltfileKey"]][
-            "uiResources"
-        ] = status_json["uiResources"]
+        # Query the project info, if available
+        for pkey in self.project_info:
+            status_json = self.get_tilt_status(self.project_info[pkey]['port'])
+            if status_json:
+                self.project_info[pkey]["uiResources"] = status_json[
+                    "uiResources"]
+                self.project_info[pkey]["service_online"] = True
+            else:
+                self.project_info[pkey]["uiResources"].clear()
+                self.project_info[pkey]["service_online"] = False
+
+        # Set the tree nodes for all projects
         tree = self.query_one(Tree)
         tree.root.set_label("Projects")
         self.add_treedata(tree.root, self.project_info)
@@ -101,16 +145,22 @@ class TTorkApp(App):
 
     def get_tilt_status(self, port: int) -> dict:
         tilt_url = f"http://localhost:{port}/api/view"
-        response = requests.get(tilt_url)
 
-        # Check the response status code
-        if response.status_code == 200:
-            # The request was successful
-            json_response = response.json()
-        else:
-            # The request failed
-            print(
-                "Error querying tilt status: {}".format(response.status_code)
-            )
+        try:
+            response = requests.get(tilt_url)
 
-        return json_response
+            # Check the response status code
+            if response.status_code == 200:
+                # The request was successful
+                json_response = response.json()
+            else:
+                # The request failed
+                print(
+                    "Error querying tilt status: {}".format(
+                        response.status_code,
+                    )
+                )
+
+            return json_response
+        except Exception:
+            return None
